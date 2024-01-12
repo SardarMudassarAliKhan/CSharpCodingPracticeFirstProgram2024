51e6ead513e4ac952ff0a6979fd703b273b20bf4
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-13 02:35:17 +0500

    Add project files.

---
+++ BasicProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodingPracticeFirstProgram
+{
+    public class BasicProgram
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public double Gpa { get; set; }
+
+        public BasicProgram(int Id , string Name, string RollNo, double gpa)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.RollNo = RollNo;
+            this.Gpa = gpa;
+        }
+
+        public void DisplayInfo() { 
+            Console.WriteLine("Basic Program Infro:" + " Id: " + this.Id + " Name: " + this.Name + " RollNo: " + this.RollNo + " " + this.Gpa);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using CSharpCodingPracticeFirstProgram;
+
+//Input for taking input from user for Id
+Console.WriteLine("Enter Student Id");
+int Id=Convert.ToInt32(Console.ReadLine());
+
+// Input for taking the name of student from user
+Console.WriteLine("Enter Student Name");
+string Name = Console.ReadLine();
+
+// Input for taking the input for RollNo 
+Console.WriteLine("Enter Student Name RollNo");
+string RollNo = Console.ReadLine();
+
+// Input for taking the input for Gpa 
+Console.WriteLine("Enter Student Name Gpa");
+double Gpa = Convert.ToDouble(Console.ReadLine());
+
+BasicProgram basicProgram = new BasicProgram(Id, Name, RollNo, Gpa);
+basicProgram.DisplayInfo();
